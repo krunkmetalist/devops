b38e0f28c6545c142b671da3ef7d117b8f1e5f41
krunkmetalist <krunkmetalist@gmail.com>
2024-06-20 14:55:00 -0600

    Update create_admin.sh

--- create_admin.sh
+++ create_admin.sh
@@ -2,7 +2,7 @@
 
 echo "creating user..."
 groupadd gcompadmin
-useradd -g gcompadmin -G admin -s /bin/bash -d /home/compadmin compadmin -p derptest
+useradd -aG gcompadmin -s /bin/bash -d /home/compadmin compadmin -p derptest
 mkdir -p /home/compadmin
 cp -r /root/.ssh /home/compadmin/.ssh
 chown -R compadmin:gcompadmin /home/compadmin
