486ac811bcaeb58e7aae03cac64d9544b1cf4cc0
Nate <nreboiro@companioncorp.com>
2024-06-20 16:27:45 -0600

    fixing user name when writing to sudoers

--- create_admin.sh
+++ create_admin.sh
@@ -6,4 +6,4 @@ useradd -g gcompadmin -s /bin/bash -d /home/compadmin compadmin -p derptest
 mkdir -p /home/compadmin
 cp -r /root/.ssh /home/compadmin/.ssh
 chown -R compadmin:gcompadmin /home/compadmin
-echo "gcompadmin ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
+echo "compadmin ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
